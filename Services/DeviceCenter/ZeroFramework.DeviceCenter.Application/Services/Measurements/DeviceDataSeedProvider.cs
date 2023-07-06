b61a0027984bdaaa39bde7bc41264c8959bf5139
hello <stacks@outlook.com>
2023-07-06 14:26:18 +0800

    重构LOGO显示方式

--- Services/DeviceCenter/ZeroFramework.DeviceCenter.Application/Services/Measurements/DeviceDataSeedProvider.cs
+++ Services/DeviceCenter/ZeroFramework.DeviceCenter.Application/Services/Measurements/DeviceDataSeedProvider.cs
@@ -12,30 +12,15 @@ namespace ZeroFramework.DeviceCenter.Application.Services.Measurements
 
         private readonly IRepository<Device, long> _deviceRepository;
 
-        private readonly IDeviceDataApplicationService _deviceApplicationService;
-
-        private readonly ILogger<DeviceDataSeedProvider> _logger;
-
         public DeviceDataSeedProvider(IRepository<Product, int> productRepository, IRepository<Device, long> deviceRepository, IDeviceDataApplicationService deviceDataApplicationService, ILogger<DeviceDataSeedProvider> logger)
         {
             _productRepository = productRepository;
             _deviceRepository = deviceRepository;
-            _deviceApplicationService = deviceDataApplicationService;
-            _logger = logger;
         }
 
         public async Task SeedAsync(IServiceProvider serviceProvider)
         {
             await CreateDevicesAsync();
-
-            try
-            {
-                await GenerateDeviceDataAsync();
-            }
-            catch (Exception ex)
-            {
-                _logger.LogWarning(ex, "Device demo data generation failed.");
-            }
         }
 
         public async Task CreateDevicesAsync()
@@ -122,87 +107,6 @@ namespace ZeroFramework.DeviceCenter.Application.Services.Measurements
                     }
                 }
             }
-        }
-
-        public async Task GenerateDeviceDataAsync()
-        {
-            List<Product> productList = await _productRepository.GetListAsync();
-
-            List<Device> deviceList = await _deviceRepository.GetListAsync();
-
-            Random random = new(Guid.NewGuid().GetHashCode());
-
-            _logger.LogInformation("Start generating demo data...");
-
-            foreach (Device device in deviceList)
-            {
-                _logger.LogInformation("Start generating {deviceName} demo data...", device.Name);
-
-                var values = await _deviceApplicationService.GetDevicePropertyValues(device.ProductId, device.Id);
-
-                if (values is not null && values.Max(e => e.Timestamp) > DateTimeOffset.Now.AddDays(-1).ToUnixTimeMilliseconds())
-                {
-                    continue;
-                }
-
-                var properties = productList.SingleOrDefault(e => e.Id == device.ProductId)?.Features?.Properties ?? Enumerable.Empty<PropertyFeature>();
-
-                foreach (var propery in properties)
-                {
-                    DateTimeOffset startDate = DateTimeOffset.Now.AddDays(-1);
-
-                    while (startDate < DateTimeOffset.Now)
-                    {
-                        startDate = startDate.AddMinutes(random.Next(10, 20));
-
-                        DevicePropertyValue devicePropertyValue = new()
-                        {
-                            Timestamp = startDate.ToUnixTimeMilliseconds()
-                        };
-
-                        if (propery.DataType.Type is DataTypeDefinitions.Int32 or DataTypeDefinitions.Int64)
-                        {
-                            devicePropertyValue.Value = random.Next(ushort.MaxValue);
-                        }
-
-                        if (propery.DataType.Type is DataTypeDefinitions.Float or DataTypeDefinitions.Double)
-                        {
-                            if (propery.DataType.Specs is not null)
-                            {
-                                int min = Convert.ToInt32(propery.DataType.Specs.First(e => e.Key == "minValue").Value?.ToString());
-                                int max = Convert.ToInt32(propery.DataType.Specs.First(e => e.Key == "maxValue").Value?.ToString());
-                                devicePropertyValue.Value = random.Next(min, max) + Random.Shared.NextDouble() * 10;
-                            }
-                        }
-
-                        if (propery.DataType.Type is DataTypeDefinitions.Bool)
-                        {
-                            devicePropertyValue.Value = random.Next(0, 10) % 2 == 0;
-                        }
-
-                        if (propery.DataType.Type is DataTypeDefinitions.Date)
-                        {
-                            devicePropertyValue.Value = DateTimeOffset.Now.AddMilliseconds(-random.Next(int.MaxValue)).ToUnixTimeMilliseconds();
-                        }
-
-                        if (propery.DataType.Type is DataTypeDefinitions.String)
-                        {
-                            devicePropertyValue.Value = Path.GetRandomFileName();
-                        }
-
-                        if (devicePropertyValue.Value is not null)
-                        {
-                            await _deviceApplicationService.SetDevicePropertyValues(device.ProductId, device.Id,
-                                new Dictionary<string, DevicePropertyValue>
-                                {
-                                    {propery.Identifier, devicePropertyValue }
-                                });
-                        }
-                    }
-                }
-            }
-
-            _logger.LogInformation("All demo data generated.");
         }
     }
 }
--- Web/ZeroFramework.DeviceCenter.Web/config/defaultSettings.ts
+++ Web/ZeroFramework.DeviceCenter.Web/config/defaultSettings.ts
@@ -14,7 +14,7 @@ const Settings: LayoutSettings & {
   colorWeak: false,
   title: "Loading...",
   pwa: false,
-  logo: 'https://www.xcode.me/img/logo-white.svg',
+  logo: 'https://cloud.helloworldnet.com/logo-white.svg',
   iconfontUrl: '',
 };
 
